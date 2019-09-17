360e2c354b47b0dfb1d5c6dc71b03f1061c93422
Alejandro Marañés Escribano <alejandromaranes@gmail.com>
2019-09-17 13:12:10 +0200

    7

---
+++ NotaMedia2/NotaMedia2/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  NotaMedia2
+//
+//  Created by Alejandro Marañés on 17/09/2019.
+//  Copyright © 2019 Alejandro Marañés. All rights reserved.
+//
+
+import Foundation
+
+func suma(op1: Int, op2: Int, op3: Int, op4: Int, op5: Int) -> Int {
+    return op1 + op2 + op3 + op4 + op5
+}
+var resultado = suma(op1: 5, op2: 6, op3: 7, op4: 8, op5: 10)
+
