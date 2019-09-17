549e9180bad9ba78e82cb41026005d98433fba65
Alejandro Marañés Escribano <alejandromaranes@gmail.com>
2019-09-17 09:35:44 +0200

    3

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,7 +1,11 @@
 
 var numero = 3
 var numero1:Double = 2.5
+var decimal: Double = 666
 numero = 4
 print("Arriba España")
 print("mi numero es el ", numero,numero1)
+decimal = Double(numero)
+let persona = ("Lennin", 85)
+print (persona.0, persona.1)
 
