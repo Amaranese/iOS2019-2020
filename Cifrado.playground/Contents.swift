0b1a6db035b1bad27c443aba2b8f486d4779c52a
Alejandro Marañés Escribano <alejandromaranes@gmail.com>
2019-09-19 10:07:22 +0200

    mas

---
+++ Cifrado.playground/Contents.swift
@@ -0,0 +1,96 @@
+import Foundation
+var passCifrada = ""
+func cifrar(){
+    print("Introduzca la contraseña: ")
+    let contraseña = readLine()!
+    for i in contraseña{
+        if i != "a" && i != "b" && i != "c" && i != "d" && i != "e" && i != "f" && i != "g" && i != "h" && i != "i" && i != "j" && i != "k" && i != "l" && i != "m" && i != "n" && i != "ñ" && i != "o" && i != "p" && i != "q" && i != "r" && i != "s" && i != "t" && i != "u" && i != "v" && i != "w" && i != "x" && i != "y" && i != "z" {
+            passCifrada.append(i)
+            
+        }
+        if i == "a"{
+            passCifrada.append("1")
+        }
+        if i == "b"{
+            passCifrada.append("2")
+        }
+        if i == "c"{
+            passCifrada.append("3")
+        }
+        if i == "d"{
+            passCifrada.append("4")
+        }
+        if i == "e"{
+            passCifrada.append("5")
+        }
+        if i == "f"{
+            passCifrada.append("6")
+        }
+        if i == "g"{
+            passCifrada.append("7")
+        }
+        if i == "h"{
+            passCifrada.append("8")
+        }
+        if i == "i"{
+            passCifrada.append("9")
+        }
+        if i == "j"{
+            passCifrada.append("10")
+        }
+        if i == "k"{
+            passCifrada.append("11")
+        }
+        if i == "l"{
+            passCifrada.append("12")
+        }
+        if i == "m"{
+            passCifrada.append("13")
+        }
+        if i == "n"{
+            passCifrada.append("14")
+        }
+        if i == "ñ"{
+            passCifrada.append("15")
+        }
+        if i == "o"{
+            passCifrada.append("16")
+        }
+        if i == "p"{
+            passCifrada.append("17")
+        }
+        if i == "q"{
+            passCifrada.append("18")
+        }
+        if i == "r"{
+            passCifrada.append("19")
+        }
+        if i == "s"{
+            passCifrada.append("20")
+        }
+        if i == "t"{
+            passCifrada.append("21")
+        }
+        if i == "u"{
+            passCifrada.append("22")
+        }
+        if i == "v"{
+            passCifrada.append("23")
+        }
+        if i == "w"{
+            passCifrada.append("24")
+        }
+        if i == "x"{
+            passCifrada.append("25")
+        }
+        if i == "y"{
+            passCifrada.append("26")
+        }
+        if i == "z"{
+            passCifrada.append("27")
+        }
+    }
+    if contraseña == "Esternocleomastoideo"{
+        print(passCifrada)
+    }
+}
