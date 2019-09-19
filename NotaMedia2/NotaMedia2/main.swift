205dae024dc36f11586008f4feffe1918eb39c3b
Alejandro Marañés Escribano <alejandromaranes@gmail.com>
2019-09-19 09:33:39 +0200

    mas

--- NotaMedia2/NotaMedia2/main.swift
+++ NotaMedia2/NotaMedia2/main.swift
@@ -7,9 +7,13 @@
 //
 
 import Foundation
-
-func suma(op1: Int, op2: Int, op3: Int, op4: Int, op5: Int) -> Int {
-    return op1 + op2 + op3 + op4 + op5
+func notamedia(){
+    
+    print("Introduce las notas: ")
+    let input = readLine()
+    let text = input!
+    var number = Int (text)
+    if number! == 5 {
+        print("Has suspendido")
+   }
 }
-var resultado = suma(op1: 5, op2: 6, op3: 7, op4: 8, op5: 10)
-
---
+++ NotaMedia3.playground/Contents.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func notamedia(){
+    print("Introduce las notas: ")
+    let input = readLine()
+    let text = input!
+    var number = Int (text)
+    if number! == 5{
+        print("Has suspendido")
+    }
+}
