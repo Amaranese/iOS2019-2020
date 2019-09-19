7d01d8d2f19097702cc723be24eaab50cf19a482
Alejandro Marañés Escribano <alejandromaranes@gmail.com>
2019-09-19 10:20:00 +0200

    mas

---
+++ Cifrado4/Cifrado4Tests/Cifrado4Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Cifrado4Tests.swift
+//  Cifrado4Tests
+//
+//  Created by Alejandro Marañés on 19/09/2019.
+//  Copyright © 2019 Alejandro Marañés. All rights reserved.
+//
+
+import XCTest
+@testable import Cifrado4
+
+class Cifrado4Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
