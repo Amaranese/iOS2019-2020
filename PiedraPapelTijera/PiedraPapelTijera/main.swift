d2a54e5e40893263c6c6f55eda8acadabcf9adc5
Alejandro Marañés Escribano <alejandromaranes@gmail.com>
2019-09-24 09:55:19 +0200

    holl<

---
+++ PiedraPapelTijera/PiedraPapelTijera/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PiedraPapelTijera
+//
+//  Created by Alejandro Marañés on 24/09/2019.
+//  Copyright © 2019 Alejandro Marañés. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
