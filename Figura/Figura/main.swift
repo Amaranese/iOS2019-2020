e20dd8dc81af852c713b2a3b7b44efd77a197d0b
Alejandro Marañés Escribano <alejandromaranes@gmail.com>
2019-09-24 09:30:11 +0200

    .

--- Figura/Figura/main.swift
+++ Figura/Figura/main.swift
@@ -18,41 +18,53 @@ class Figura {
         posicion = (x,y)
         area = 0
     }
-}
-class Cuadrado: Figura {
-    public let lado: Float
-    init(lado: Float) {
-        if lado < 0 {
-            self.lado = 1
-        } else {
-            self.lado = lado
-    }
-        super.init()
-        area = self.lado * self.lado
-    }
-}
-class Circulo: Figura {
-    public let radio: Float
-    init(radio: Float) {
-        if radio < 0 {
-            self.radio = 1
-        } else {
-            self.radio = radio
+    class Cuadrado: Figura {
+        public let lado: Float
+        init(lado: Float) {
+            if lado < 0 {
+                self.lado = 1
+            } else {
+                self.lado = lado
+            }
+            super.init()
+            area = self.lado * self.lado
         }
-        super.init()
-        area = Float.pi * pow(radio, 2)
-    }
-}
-class Triangulo: Figura {
-    public let lado:Float
-    init(lado:Float){
-        if lado < 0 {
-            self.lado = 1
-        } else {
-            self.lado = lado
+        class Circulo: Figura {
+            public let radio: Float
+            init(radio: Float) {
+                if radio < 0 {
+                    self.radio = 1
+                } else {
+                    self.radio = radio
+                }
+                super.init()
+                area = Float.pi * pow(radio, 2)
+            }
+            class Triangulo: Figura {
+                public let lado:Float
+                init(lado:Float){
+                    if lado < 0 {
+                        self.lado = 1
+                    } else {
+                        self.lado = lado
+                    }
+                    super.init()
+                    area = self.lado * self.lado / 2
+                }
+                class Rectangulo: Figura {
+                    public let lado: Float
+                    init(lado: Float) {
+                        if lado < 0 {
+                            self.lado = 1
+                        } else {
+                            self.lado = lado
+                        }
+                        super.init()
+                        area = self.lado * self.lado
+                    }
+                }
+            }
         }
-        super.init()
-        area = self.lado * self.lado / 2
     }
 }
 
