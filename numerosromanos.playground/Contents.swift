4679ad8bc1e03941deaa556a3b30894ce4e7a506
Alejandro Marañés Escribano <alejandromaranes@gmail.com>
2019-09-27 11:30:18 +0200

    more

--- numerosromanos.playground/Contents.swift
+++ numerosromanos.playground/Contents.swift
@@ -7,7 +7,7 @@ extension Int {
         var cadena = ""
         var numero = self
         if numero > 1000 {
-            for i in 0..<(numero / 1000) {
+            for _ in 0..<(numero / 1000) {
                 cadena += "M"
             }
         }
@@ -55,8 +55,7 @@ extension Int {
         return "Por implementar"
     }
 }
-let cero = 0.romano
-let uno = 1.romano
+
 let dos = 2.romano
 let tres = 3.romano
 let cuatro = 4.romano
@@ -66,7 +65,6 @@ let siete = 7.romano
 let ocho = 8.romano
 let nueve = 9.romano
 let diez = 10.romano
-let once = 11.romano
 let doce = 12.romano
 let trece = 13.romano
 let catorce = 14.romano
@@ -76,67 +74,8 @@ let dicisiete = 17.romano
 let diciocho = 18.romano
 let diecinueve = 19.romano
 let veinte = 20.romano
+let veintidos = 22.romano
 
-let unoArray = uno.map {
-    String($0)
-}
-let dosArray = dos.map {
-    String($0)
-}
-let tresArray = tres.map {
-    String($0)
-}
-let cuatroArray = cuatro.map {
-    String($0)
-}
-let cincoArray = cinco.map {
-    String($0)
-}
-let seisArray = seis.map {
-    String($0)
-}
-let sieteArray = siete.map {
-    String($0)
-}
-let ochoArray = ocho.map {
-    String($0)
-}
-let nueveArray = nueve.map {
-    String($0)
-}
-let diezArray = diez.map {
-    String($0)
-}
-let onceArray = once.map {
-    String($0)
-}
-let doceArray = doce.map {
-    String($0)
-}
-let treceArray = trece.map {
-    String($0)
-}
-let catorceArray = catorce.map {
-    String($0)
-}
-let quinceArray = quince.map {
-    String($0)
-}
-let dieciseisArray = diciseis.map {
-    String($0)
-}
-let dicisieteArray = dicisiete.map {
-    String($0)
-}
-let diciochoArray = diciocho.map {
-    String($0)
-}
-let dicinueveArray = diecinueve.map {
-    String($0)
-}
-let veinteArray = veinte.map {
-    String($0)
-}
 
 
 
