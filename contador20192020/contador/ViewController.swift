358833624ffdbb321c546b61dcfeb224a84b5054
Alejandro Marañés Escribano <alejandromaranes@gmail.com>
2019-10-04 11:56:15 +0200

    more

--- contador20192020/contador/ViewController.swift
+++ contador20192020/contador/ViewController.swift
@@ -11,11 +11,11 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         }
     @IBAction func sumar(_ sender: AnyObject) {
-        i = i + 1
+        i = i + 2
         self.contado.text = String(i)
     }
     @IBAction func restar(_ sender: AnyObject) {
-        i = i - 1
+        i = i - 2
         self.contado.text = String(i)
     }
     @IBAction func multiplicar(_ sender: AnyObject) {
@@ -26,16 +26,14 @@ class ViewController: UIViewController {
         i = i / numeros
         self.contado.text = String(i)
     }
-//    @IBAction func numerousuario(_ sender: UITextField) {
-//        let numero = Int(sender.text!)
-//
-//        if let n = numero{
-//            i = n;
-//            contado.text = String(i)
-//            sender.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-//        } else {
-//            sender.textColor = #colorLiteral(red: 1, green: 0.1273401127, blue: 0, alpha: 1)
-//        }
-//    }
-//}
+    @IBAction func numerodeusuario(_ sender: UITextField) {
+        let numero = Int(sender.text!)
+        if let n = numero {
+            i = n;
+            contado.text = String(i)
+            sender.textColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
+        } else {
+            sender.textColor = #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1)
+        }
+    }
 }
